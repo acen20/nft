ecd9fc79bf678ad8a0643cba585347fc385f84cd
acen20 <ahsen1330@gmail.com>
2021-09-19 16:11:24 +0500

    random push

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("1");
+var hamburger = document.querySelector(".hamburger");
+var navlinks = document.querySelector(".nav-links");
+var body = document.querySelector("body");
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("condense");
+  body.classList.toggle("disablePointer");
+  navlinks.classList.toggle("display");
+});
